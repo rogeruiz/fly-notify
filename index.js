1776ed917bac000890b751001d9fe31bf911dc25
Roger Steve Ruiz <hi@rog.gr>
2015-07-29 01:08:15 -0400

    Add message support and clean up index a little

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-const path = require("path")
-const assign = require("object-assign")
-const notifier = require("node-notifier")
+const path = require( "path" )
+const assign = require( "object-assign" )
+const notifier = require( "node-notifier" )
 
 module.exports = function () {
-  this.notify = function (options) {
-    notifier.notify(assign(options, {
-      title: "Fly",
-      message: "Task completed.",
-      icon: options.icon
-        ? devicon(options.icon)
-        : path.join(__dirname, "fly-logo.png")
-    }),
-    function (error, response) {
-      if (error) this.error(error)
-      if (response) this.debug(response)
-    }.bind(this))
+
+  this.notify = function ( options ) {
+
+    var defaults = {
+
+      title: 'Fly',
+      message: 'Task completed.',
+      icon: path.join( __dirname, 'fly-logo.png' ),
+
+    }
+
+    if ( options.icon ) {
+
+      options.icon = devicon( options.icon )
+
+    }
+
+    notifier.notify( assign( defaults, options ),
+
+    function ( error, response ) {
+
+      if ( error ) this.error( error )
+
+      if ( response ) this.debug( response )
+
+    }.bind( this ) )
+
     return this
+
   }
+
 }
 
-function devicon (s) {
-  const ICONS_PATH = "node_modules/fly-notify/node_modules/devicons/!SVG/"
-  return s.slice(0, 3) === "dev"
-    ? path.join(process.cwd(), ICONS_PATH, s.slice(4) + ".svg") : s
+function devicon ( s ) {
+
+  const ICONS_PATH = "node_modules/fly-notify/node_modules/devicons/!PNG/"
+  let icon = path.join( process.cwd(), ICONS_PATH, s.slice( 4 ) + ".png" )
+
+  return s.slice( 0, 3 ) === "dev" ? icon: s
+
 }
